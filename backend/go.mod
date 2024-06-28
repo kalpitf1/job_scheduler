10010295a07a4abcf0d558d1d451a74fdaea48f8
kalpitfulwariya <kalpit.fulwariya@kaleidoscope-blockchain.com>
2024-06-28 19:09:24 +0530

    feat: add rest apis for add and get jobs

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/kalpitf1/job_scheduler/backend
 
 go 1.22.4
+
+require github.com/gorilla/mux v1.8.1
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+// Job represents data about a posted job
+type Job struct {
+	Name     string        `json:"name"`
+	Duration time.Duration `json:"duration"`
+	Status   string        `json:"status"`
+}
+
+var jobs []Job
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello from Golang server!"))
-    })
-    http.ListenAndServe(":8080", nil)
+	log.Println("Starting Backend")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/jobs", getJobs).Methods("GET")
+	r.HandleFunc("/jobs", createJob).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getJobs(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(jobs)
+}
+
+func createJob(w http.ResponseWriter, r *http.Request) {
+	var newJob Job
+	_ = json.NewDecoder(r.Body).Decode(&newJob)
+	jobs = append(jobs, newJob)
+	json.NewEncoder(w).Encode(newJob)
 }
