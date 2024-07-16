cdc0e97ed8148ecd3086a2ad8bcff9e6fef09183
kalpitfulwariya <kalpit.fulwariya@kaleidoscope-blockchain.com>
2024-07-16 18:18:37 +0530

    tests: add CreateJob tests + benchmark, add mutexes for BroadcastEnabled

--- backend/job_handlers/job.go
+++ backend/job_handlers/job.go
@@ -14,17 +14,31 @@ import (
 )
 
 var (
-	Jobs        []*models.Job
-	JobsMutex   sync.Mutex
-	jobCounter  int
-	pq          utils.JobPriorityQueue
-	pqMutex     sync.Mutex
-	processing  bool
-	processingM sync.Mutex
+	Jobs                  []*models.Job
+	JobsMutex             sync.Mutex
+	jobCounter            int
+	pq                    utils.JobPriorityQueue
+	pqMutex               sync.Mutex
+	processing            bool
+	processingM           sync.Mutex
+	BroadcastEnabled      = true
+	BroadcastEnabledMutex sync.Mutex
 )
 
 func init() {
+	pq = make(utils.JobPriorityQueue, 0)
 	heap.Init(&pq)
+}
+
+func Reset() {
+	pqMutex.Lock()
+	defer pqMutex.Unlock()
+	pq = make(utils.JobPriorityQueue, 0)
+	heap.Init(&pq)
+	Jobs = make([]*models.Job, 0)
+	jobCounter = 0
+	processing = false
+	BroadcastEnabled = false
 }
 
 func GetJobs(w http.ResponseWriter, r *http.Request) {
@@ -41,12 +55,18 @@ func GetJobs(w http.ResponseWriter, r *http.Request) {
 
 func CreateJob(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	// log.Println("CreateJob: Start")
+
 	var newJob models.Job
 	err := json.NewDecoder(r.Body).Decode(&newJob)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		// log.Println("CreateJob: Error decoding request body:", err)
 		return
 	}
+
+	// log.Printf("CreateJob: Received job - %+v\n", newJob)
 
 	newJob.Status = "Pending"
 	newJob.ID = jobCounter
@@ -60,7 +80,20 @@ func CreateJob(w http.ResponseWriter, r *http.Request) {
 	heap.Push(&pq, &newJob)
 	pqMutex.Unlock()
 
-	websocket.Broadcast <- &newJob
+	BroadcastEnabledMutex.Lock()
+	// log.Printf("CreateJob: BroadcastEnabled is %v\n", BroadcastEnabled)
+	// broadcast enabled by default, can be turned off for testing
+	if BroadcastEnabled {
+		// websocket.Broadcast <- &newJob
+
+		select {
+		case websocket.Broadcast <- &newJob:
+			log.Println("CreateJob: Broadcast successful")
+		default:
+			log.Println("CreateJob: Broadcast channel is full, skipping broadcast")
+		}
+	}
+	BroadcastEnabledMutex.Unlock()
 
 	// Wake up the processing goroutine if it's idle
 	processingM.Lock()
@@ -75,8 +108,11 @@ func CreateJob(w http.ResponseWriter, r *http.Request) {
 	err = json.NewEncoder(w).Encode(newJob)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		// log.Println("CreateJob: Error encoding response:", err)
 		return
 	}
+
+	// log.Println("CreateJob: End")
 }
 
 func processJobs() {
@@ -90,23 +126,61 @@ func processJobs() {
 			return
 		}
 
+		// BroadcastEnabledMutex.Lock()
+		// log.Printf("BroadcastEnabled before heap pop: %v", BroadcastEnabled)
+		// BroadcastEnabledMutex.Unlock()
 		job := heap.Pop(&pq).(*models.Job)
+		// BroadcastEnabledMutex.Lock()
+		// log.Printf("BroadcastEnabled after heap pop: %v", BroadcastEnabled)
+		// BroadcastEnabledMutex.Unlock()
 		pqMutex.Unlock()
 
 		JobsMutex.Lock()
+		// BroadcastEnabledMutex.Lock()
+		// log.Printf("BroadcastEnabled before changing job status to In Progress: %v", BroadcastEnabled)
+		// BroadcastEnabledMutex.Unlock()
 		job.Status = "In Progress"
 		JobsMutex.Unlock()
 		log.Printf("Processing job: %+v\n", job)
 
-		websocket.Broadcast <- job
+		BroadcastEnabledMutex.Lock()
+		// broadcast enabled by default, can be turned off for testing
+		if BroadcastEnabled {
+			// websocket.Broadcast <- job
 
+			select {
+			case websocket.Broadcast <- job:
+				log.Println("CreateJob: Broadcast successful")
+			default:
+				log.Println("CreateJob: Broadcast channel is full, skipping broadcast")
+			}
+		}
+		BroadcastEnabledMutex.Unlock()
+
+		// log.Printf("Sleeping for %+v", job.Duration)
 		time.Sleep(job.Duration)
+		// log.Printf("Done sleeping for %+v", job.Duration)
 
 		JobsMutex.Lock()
+		// BroadcastEnabledMutex.Lock()
+		// log.Printf("BroadcastEnabled before changing job status to Completed: %v", BroadcastEnabled)
+		// BroadcastEnabledMutex.Unlock()
 		job.Status = "Completed"
 		JobsMutex.Unlock()
 		log.Printf("Completed job: %+v\n", job)
 
-		websocket.Broadcast <- job
+		BroadcastEnabledMutex.Lock()
+		// broadcast enabled by default, can be turned off for testing
+		if BroadcastEnabled {
+			// websocket.Broadcast <- job
+
+			select {
+			case websocket.Broadcast <- job:
+				log.Println("CreateJob: Broadcast successful")
+			default:
+				log.Println("CreateJob: Broadcast channel is full, skipping broadcast")
+			}
+		}
+		BroadcastEnabledMutex.Unlock()
 	}
 }
--- backend/job_handlers/job_test.go
+++ backend/job_handlers/job_test.go
@@ -1,6 +1,7 @@
 package job_handlers_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -10,12 +11,15 @@ import (
 	"github.com/gorilla/mux"
 	job_handlers "github.com/kalpitf1/job_scheduler/backend/job_handlers"
 	"github.com/kalpitf1/job_scheduler/backend/models"
+	"github.com/kalpitf1/job_scheduler/backend/websocket"
 )
 
 // Create a new mux router and register the GetJobs handler.
 func setupRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/jobs", job_handlers.GetJobs).Methods("GET")
+	r.HandleFunc("/jobs", job_handlers.CreateJob).Methods("POST")
+	r.HandleFunc("/ws", websocket.HandleConnections)
 	return r
 }
 
@@ -35,7 +39,8 @@ func addTestJobs(numOfJobs int) {
 }
 
 func TestGetJobs(t *testing.T) {
-	// job_handlers.Reset()
+	// Initialize the handler package's state
+	job_handlers.Reset()
 
 	// Setup: Initialize the jobs slice with 2 jobs.
 	numOfJobs := 2
@@ -80,6 +85,65 @@ func TestGetJobs(t *testing.T) {
 	}
 }
 
+func TestCreateJob(t *testing.T) {
+	// Initialize the handler package's state
+	job_handlers.Reset()
+
+	// Disable broadcasting for the test
+	job_handlers.BroadcastEnabledMutex.Lock()
+	job_handlers.BroadcastEnabled = false
+	job_handlers.BroadcastEnabledMutex.Unlock()
+	defer func() {
+		job_handlers.BroadcastEnabledMutex.Lock()
+		job_handlers.BroadcastEnabled = true
+		job_handlers.BroadcastEnabledMutex.Unlock()
+
+	}() // Ensure it's re-enabled after the test
+
+	done := make(chan bool)
+	go func() {
+		newJob := models.Job{
+			Name:     "Job 1",
+			Duration: time.Duration(1) * time.Second,
+		}
+
+		body, err := json.Marshal(newJob)
+		if err != nil {
+			t.Fatalf("Failed to marshal job: %v", err)
+		}
+
+		// Create a new HTTP request to the /jobs endpoint.
+		req, err := http.NewRequest("POST", "/jobs", bytes.NewBuffer(body))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Create a ResponseRecorder to capture the response.
+		w := httptest.NewRecorder()
+
+		// Setup the router for API calls
+		r := setupRouter()
+
+		// Serve the HTTP request.
+		// log.Println("TestCreateJob: Calling ServeHTTP")
+		r.ServeHTTP(w, req)
+		// log.Println("TestCreateJob: ServeHTTP returned")
+
+		// Check the status code.
+		if status := w.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+		done <- true
+	}()
+
+	select {
+	case <-done:
+	// Test completed successfully
+	case <-time.After(10 * time.Second):
+		t.Fatal("TestCreateJob timed out")
+	}
+}
+
 func BenchmarkGetJobs(b *testing.B) {
 	// Add numOfJobs jobs for the benchmark
 	numOfJobs := 1000
@@ -117,6 +181,67 @@ func BenchmarkGetJobs(b *testing.B) {
 
 		if len(responseJobs) != numOfJobs {
 			b.Errorf("handler returned unexpected number of jobs: got %v want %v", len(responseJobs), 2)
+		}
+	}
+}
+
+func BenchmarkCreateJob(b *testing.B) {
+	// Initialize the handler package's state
+	job_handlers.Reset()
+
+	// Disable broadcasting for the test
+	job_handlers.BroadcastEnabledMutex.Lock()
+	job_handlers.BroadcastEnabled = false
+	job_handlers.BroadcastEnabledMutex.Unlock()
+
+	defer func() {
+		job_handlers.BroadcastEnabledMutex.Lock()
+		job_handlers.BroadcastEnabled = true
+		job_handlers.BroadcastEnabledMutex.Unlock()
+
+	}() // Ensure it's re-enabled after the test
+
+	newJob := models.Job{
+		Name:     "Job 1",
+		Duration: time.Duration(1) * time.Second,
+	}
+
+	body, err := json.Marshal(newJob)
+	if err != nil {
+		b.Fatalf("Failed to marshal job: %v", err)
+	}
+
+	// Create a new HTTP request to the /jobs endpoint.
+	// req, err := http.NewRequest("POST", "/jobs", bytes.NewBuffer(body))
+	// if err != nil {
+	// 	b.Fatal(err)
+	// }
+
+	// Setup the router for API calls
+	r := setupRouter()
+
+	// Reset timer due to expensive setup
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		job_handlers.Reset()
+		job_handlers.BroadcastEnabledMutex.Lock()
+		job_handlers.BroadcastEnabled = false
+		job_handlers.BroadcastEnabledMutex.Unlock()
+
+		req, err := http.NewRequest("POST", "/jobs", bytes.NewBuffer(body))
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		// Create a ResponseRecorder to capture the response.
+		w := httptest.NewRecorder()
+
+		// Serve the HTTP request.
+		r.ServeHTTP(w, req)
+
+		// Check the status code.
+		if status := w.Code; status != http.StatusOK {
+			b.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 		}
 	}
 }
